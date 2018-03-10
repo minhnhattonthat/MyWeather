891733a13f69dbaedd4603d21c7153f46eefdca6
Nhat Ton <minhnhattonthat@gmail.com>
2018-03-10 23:28:14 +0800

    initial commit

---
+++ app/src/main/java/com/nhatton/myweather/MainActivity.java
@@ -0,0 +1,25 @@
+package com.nhatton.myweather;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CityListActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
