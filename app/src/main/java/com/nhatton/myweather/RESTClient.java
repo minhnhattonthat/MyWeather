5476880f6883a6fcd63c01f3eeb89f5baccc7de8
Nhat Ton <minhnhattonthat@gmail.com>
2018-03-11 00:58:28 +0800

    Add http client

---
+++ app/src/main/java/com/nhatton/myweather/RESTClient.java
@@ -0,0 +1,74 @@
+package com.nhatton.myweather;
+
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+/**
+ * Created by nhatton on 10/3/18.
+ */
+
+public class RESTClient {
+
+    private static final String TAG = "RESTClient";
+
+    public RESTClient() {
+
+    }
+
+    public String get(String reqUrl) {
+        String response = null;
+        HttpURLConnection conn = null;
+        try {
+            URL url = new URL(reqUrl);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            // read the response
+            InputStream inputStream = new BufferedInputStream(conn.getInputStream());
+            response = convertStreamToString(inputStream);
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "MalformedURLException: " + e.getMessage());
+        } catch (ProtocolException e) {
+            Log.e(TAG, "ProtocolException: " + e.getMessage());
+        } catch (IOException e) {
+            Log.e(TAG, "IOException: " + e.getMessage());
+        } catch (Exception e) {
+            Log.e(TAG, "Exception: " + e.getMessage());
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+
+        return response;
+    }
+
+    private static String convertStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                Log.e(TAG, "convertStreamToString: ", e);
+                ;
+            }
+        }
+        return sb.toString();
+    }
+}
---
+++ app/src/main/java/com/nhatton/myweather/WeatherAPI.java
@@ -0,0 +1,12 @@
+package com.nhatton.myweather;
+
+/**
+ * Created by nhatton on 11/3/18.
+ */
+
+public class WeatherAPI {
+
+    private static final String API_KEY = "1e7f27cf98fac44b02286ba9e69c4173";
+
+    private static final String ROOT_URL = "http://api.openweathermap.org/data/2.5/weather";
+}
